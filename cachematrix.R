02b00495d374e6531f511d48d4faaa04bde610fe
Balinda Strosnider <Balinda@users.noreply.github.com>
2016-09-19 06:28:54 -0700

    Just making sure the commit goes to MY directory

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week 3 of R Programming.
+## These functions....
+## 
 
 ## Write a short comment describing this function
 
